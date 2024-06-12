b53c4019ef7b65935ea1ca2ab166f75470bf38c0
yelkov <yelkovq@gmail.com>
2024-06-12 20:16:37 +0200

    feat(operaciones.py): implementar funciones basicas para operar

---
+++ src/operaciones/operaciones.py
@@ -0,0 +1,12 @@
+
+def sumar(primer_operador, segundo_operador):
+    return primer_operador + segundo_operador
+
+def restar(primer_operador, segundo_operador):
+    return primer_operador - segundo_operador
+
+def multiplicar(primer_operador,segundo_operador):
+    return primer_operador * segundo_operador
+
+def dividir(primer_operador, segundo_operador):
+    return primer_operador / segundo_operador
--- test/test_operaciones.py
+++ test/test_operaciones.py
@@ -1,7 +1,25 @@
 import pytest
-from src.operaciones import *
+from src.operaciones.operaciones import *
 
 def test_suma():
     assert sumar(1,2) == 3
     assert sumar(-1,1) == 0
     assert sumar(-1,-1) == -2
+    assert sumar(5,0) == 5
+
+def test_resta():
+    assert restar(8,5) == 3
+    assert restar(3,3) == 0
+    assert restar(-2,2) == -4
+    assert restar(5,0) == 5
+
+def test_multiplicar():
+    assert multiplicar(2,2) == 4
+    assert multiplicar(7,1) == 7
+    assert multiplicar(5,-3) == -15
+    assert multiplicar(5,0) == 0
+
+def test_dividir():
+    assert dividir(10,2) == 5
+    assert dividir(0,5) == 0
+    assert dividir(100,-10) == -10
